8afb49c5f9aae2bd9f9fc573fda4ee72a392abe4
Zachary Felix <zf2153@messiah.edu>
2014-03-21 01:49:20 +0000

    Fixed various problems

--- ScanRoom.rb
+++ ScanRoom.rb
@@ -15,21 +15,21 @@ def scan_room(wi, pl, room, verbose = false)
 		end
 	end
 	for vote in voteposts
-		matches = pl.match(vote[0], room.players, verbose)
-		next if matches.length != 1
-		voter = matches[0]
+		matches = pl.get_player(vote[0], room.players, verbose)
+		next unless matches
+		voter = matches
 		matches = vote[1].match(/vote ([^ ,]+)/i)
 		next unless matches
 		next unless matches.length > 1
-		matches = pl.match(matches[1], room.players, verbose)
-		next if matches.length != 1
-		votee = matches[0]
+		matches = pl.get_player(matches[1], room.players, verbose)
+		next unless matches
+		votee = matches
 
 		lock = false
 		lock = true if vote[1].downcase.include?("lock vote")
 		lock = true if vote[1].downcase.include?("lockvote")
 
+		puts "#{pl[voter].name} #{lock ? "lock" : ""}votes for #{pl[votee].name}" if verbose
 		room.vote(voter, votee, lock)
-		# puts "#{pl[voter].name} votes for #{pl[votee].name}"
 	end
 end
